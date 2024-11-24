faaefd6fb64451515394751663990e5698f2d6b4
György Viktoria Anna <szt3gyovik@vasvari.org>
2024-11-24 16:23:02 +0100

    igen

--- ceci/ceci/ViewModels/UserViewModel.cs
+++ ceci/ceci/ViewModels/UserViewModel.cs
@@ -4,13 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using UserProject.Models;
 
 namespace ceci.ViewModels
 {
   
-    public partial class StudentViewModel : ObservableObject
+    public partial class UserViewModel : ObservableObject
     {
         [ObservableProperty]
         private User _user;
+
+        public UserViewModel()
+        {
+            _user = new User();
+        }
     }
 }
