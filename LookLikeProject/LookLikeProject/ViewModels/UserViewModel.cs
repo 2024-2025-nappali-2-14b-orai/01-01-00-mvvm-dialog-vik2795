fb12d3d3e51f657de768acfa8baeda355480079b
vik2795 <157577386+vik2795@users.noreply.github.com>
2024-11-25 13:56:40 +0100

    UserViewModel kész

--- LookLikeProject/LookLikeProject/ViewModels/UserViewModel.cs
+++ LookLikeProject/LookLikeProject/ViewModels/UserViewModel.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using LookLikeProject.Models;
+using System.Collections.Generic;
 
 namespace LookLikeProject.ViewModels
 {
     public partial class UserViewModel:ObservableObject
     {
         [ObservableProperty]
+        private User _user;
+
+
+        public UserViewModel() 
+        {
+            _user= new User();
+        }
     }
 }
