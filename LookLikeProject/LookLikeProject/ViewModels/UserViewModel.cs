9c56b7be17163aa5bd48679435979f99cbdb37bc
vik2795 <157577386+vik2795@users.noreply.github.com>
2024-11-25 14:23:09 +0100

    Buttons

--- LookLikeProject/LookLikeProject/ViewModels/UserViewModel.cs
+++ LookLikeProject/LookLikeProject/ViewModels/UserViewModel.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using LookLikeProject.Models;
 using System.Collections.Generic;
+using CommunityToolkit.Mvvm.Input;
 
 namespace LookLikeProject.ViewModels
 {
@@ -14,10 +15,37 @@ namespace LookLikeProject.ViewModels
         [ObservableProperty]
         private User _user;
 
-
-        public UserViewModel() 
+        [ObservableProperty]
+        private User _student = new User();
+        public UserViewModel()
         {
-            _user= new User();
+            User.Szuldatum = DateTime.Now.AddYears(-14);
+        }
+
+        [RelayCommand]
+        public void DoSave(User user)
+        {
+            User.Add(user);
+            User = new User();
+            User.Szuldatum = DateTime.Now.AddYears(-14);
+            OnPropertyChanged(nameof(User));
+        }
+
+        [RelayCommand]
+        public void DoNewStudent()
+        {
+            User = new User();
+            User.Szuldatum = DateTime.Now.AddYears(-14);
+            OnPropertyChanged(nameof(User));
+        }
+
+        [RelayCommand]
+        public void DoDelete(User user)
+        {
+            User.Remove(user);
+            User = new User();
+            User.Szuldatum = DateTime.Now.AddYears(-14);
+            OnPropertyChanged(nameof(User));
         }
     }
 }
