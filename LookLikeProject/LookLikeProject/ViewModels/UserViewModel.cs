f107dfd1e42bf9c19cceaca4836bc1eec2a0e6d8
vik2795 <157577386+vik2795@users.noreply.github.com>
2024-11-27 14:26:47 +0100

    jó

--- LookLikeProject/LookLikeProject/ViewModels/UserViewModel.cs
+++ LookLikeProject/LookLikeProject/ViewModels/UserViewModel.cs
@@ -1,31 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CommunityToolkit.Mvvm.ComponentModel;
-using LookLikeProject.Models;
-using System.Collections.Generic;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using LookLikeProject.Models;
 using System.Collections.ObjectModel;
 
 namespace LookLikeProject.ViewModels
 {
     public partial class UserViewModel:ObservableObject
     {
-        [ObservableProperty]
-        private User _user=;
 
         [ObservableProperty]
-        private List<User> _users = 
+        private User _user = new User();
+
+       
+
+        [ObservableProperty]
+        private ObservableCollection<User> _users = new ObservableCollection<User>();
+
+        [ObservableProperty]
+        private User _selectedUser = new User();
 
         public UserViewModel()
         {
             User.Szuldatum = DateTime.Now.AddYears(-14);
         }
-
-        [ObservableProperty]
-        private ObservableCollection<User> _users = new ObservableCollection<User>();
 
         [RelayCommand]
         public void DoSave(User user)
